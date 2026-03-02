00c4eda2a862a521156382ddc7c36e6b55979804
Monavli <dahiya.monavli5@gmail.com>
2026-03-02 15:24:57 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,20 +2,11 @@ import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a word: ");
-        String input = scanner.nextLine();
+        public static void main(String[] args) {
+            System.out.println("Welcome to the Palindrome Checker Management System");
+            System.out.println("Version : 1.0");
+            System.out.println("System initialized successfully.");
 
-        String reversed = new StringBuilder(input).reverse().toString();
-
-        if (input.equalsIgnoreCase(reversed)) {
-            System.out.println("✅ It is a palindrome!");
-        } else {
-            System.out.println("❌ Not a palindrome.");
-        }
-
-        scanner.close();
     }
 }
